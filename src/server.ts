932b37ecb4cfd6ad4c0531ec9f3fa8705d946c6f
paullohdiniz <paullohdiniz@gmail.com.br>
2021-02-22 22:51:43 -0300

    Modificação de msggit add .!

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import express from 'express';
 const app = express();
 
 app.get("/users", (request,responde)=>{
-    return responde.send("Hello world");
+    return responde.send("GET serviço de usuário!!!");
 });
 
 app.post("/users", (request,responde)=>{
-    return responde.json({messagem: "Hello world"});
+    return responde.json({messagem: "POST request de serviço"});
 });
 
 app.listen(3333, () => console.log("Server ativo!!!!"));    
